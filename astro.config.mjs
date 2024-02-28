ed3bfa6cfa6e6c237df5bc3142a6083440a7498a
Tommytwotoes06 <thomashawkins2006@gmail.com>
2024-02-28 17:47:06 +0000

    english now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
@@ -6,9 +6,12 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [    tailwind({
-    applyBaseStyles: false,
-  }), react()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -29,4 +29,4 @@ export default {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
